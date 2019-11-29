6eef925a6595e9ac5efa6a5a06610a2af4613dea
razvanmorea <razvanmorea@yahoo.ro>
2019-11-29 18:53:55 +0200

    minor changes

--- ubbprotool/src/main/java/com/chk/ubbprotool/model/TeacherEntity.java
+++ ubbprotool/src/main/java/com/chk/ubbprotool/model/TeacherEntity.java
@@ -1,29 +1,24 @@
 package com.chk.ubbprotool.model;
 
-import com.sun.xml.bind.v2.model.core.ID;
 import lombok.AllArgsConstructor;
 import lombok.Data;
-import lombok.EqualsAndHashCode;
 import lombok.NoArgsConstructor;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
-import javax.persistence.Id;
 import javax.persistence.Table;
 
 @Entity
 @Table(name="Teacher")
 @Data
-@EqualsAndHashCode(callSuper = true)
 @AllArgsConstructor
 @NoArgsConstructor
 public class TeacherEntity extends BaseUserEntity {
-    public static final String ID_COLUMN = "thesis_capacity";
+    public static final String THESIS_CAPACITY = "thesis_capacity";
     public static final String DEPARTAMENT = "departament";
 
-    @Id
-    @Column(name = ID_COLUMN)
-    private Long id;
+    @Column(name = THESIS_CAPACITY)
+    private Integer thesisCapacity;
 
     @Column(name = DEPARTAMENT)
     private String departament;
