9ea8a149f03a9453b0175de3143d30ffeb135d1d
MuBirhan <birhan.muradasil@rodeapps.com>
2020-01-18 11:41:09 +0200

    small chnages

--- ubbprotool-pc/src/main/java/com/chk/ubbprotool/ubbprotool/Helper/AppContext.java
+++ ubbprotool-pc/src/main/java/com/chk/ubbprotool/ubbprotool/Helper/AppContext.java
@@ -44,7 +44,7 @@ public class AppContext {
         dataSource.setDriverClassName("com.mysql.cj.jdbc.Driver");
         dataSource.setUrl("jdbc:mysql://127.0.0.1:3306/ubbprotool");
         dataSource.setUsername("root");
-        dataSource.setPassword("98blionlion");
+        dataSource.setPassword("parola12");
         return dataSource;
     }
 
